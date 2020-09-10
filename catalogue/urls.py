9f9a30d7a6d7bfa6fc5bef2169179ff3b9b4c672
wendyorengo <wenndyorengo@gmail.com>
2020-09-10 15:29:47 +0300

    add a detail redirection, style the detail page and configure the urls

--- catalogue/urls.py
+++ catalogue/urls.py
@@ -4,7 +4,7 @@ from .views import product_list,product,product_details
 urlpatterns= [
     path('products/', product_list, name="product-list"),
     path('image/', product, name="product"),
-    path('description/', product_details, name="detail")
+    path('products/<int:product_id>/', product_details, name="detail")
 
 ]
     
--- catalogue/views.py
+++ catalogue/views.py
@@ -10,9 +10,9 @@ def product(request):
     product_image = Product.objects.all()
     return render(request, 'product.html',{'product_image':product_image})
 
-def product_details(request):
-    description = Product.objects.all()
-    return render(request, 'details.html', {'description':description})
+def product_details(request,product_id):
+    products = Product.objects.filter(id=product_id)
+    return render(request, 'details.html', {'products':products})
 
 
     
